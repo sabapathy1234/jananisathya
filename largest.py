35c55e79ba789de1ca55370e606f54add8b68c98
Jananisathya <52281059+Jananisathya@users.noreply.github.com>
2019-08-10 22:18:07 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,12 @@
+num1 = float(input(" 1"))
+num2 = float(input(" 2 "))
+num3 = float(input(" 3 "))
+ 
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num2 > num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print("largest number is",largest)
